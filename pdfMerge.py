1234fb36b7a18c93bd8d86121259625ca4e4e6be
Gregory Klassen <kl@ssen.org>
2015-01-28 15:45:14 +0000

    better file name interface

--- pdfMerge.py
+++ pdfMerge.py
@@ -1,6 +1,5 @@
 # Merge back PDFs
 import sys
-from pyPdf import PdfFileReader, PdfFileWriter
  
 """
 The code currently expects the first argument to be the 
@@ -10,40 +9,70 @@ the odd pages and 2) <base name> + " (back).pdf" that
 contains the (reverse ordered) even pages. The result
 will be a file called <base name>+".pdf" that is the 
 merged PDF file.
-"""
 baseFile = "2014 Encompass Auto Policy Info"
 baseFile = sys.argv[1]
 backFile = baseFile  + " (back).pdf"
 frontFile = baseFile +" (front).pdf"
+"""
 
-output = PdfFileWriter()
-try:
-  pdfFrnt = PdfFileReader(file( frontFile, "rb"))
-except:  
-  frontFile = baseFile +" (Front).pdf"
-  pdfFrnt = PdfFileReader(file( frontFile, "rb"))
+def baseNameToNamingConvention(baseName):
+  frontFile = baseName +" (front).pdf"
+  try:
+    file (frontFile, "rb")
+  except:  
+    frontFile = baseName +" (Front).pdf"
 
-try:
-  pdfBack = PdfFileReader(file( backFile, "rb"))
-except:  
-  backFile = baseFile  + " (Back).pdf"
-  pdfBack = PdfFileReader(file( backFile, "rb"))
+  try:
+    file( frontFile, "rb")
+  except:
+    print "no corresponding front file"
+    return "","",""
 
+  backFile = baseName +" (back).pdf"
+  try:
+    file (backFile, "rb")
+  except:  
+    backFile = baseName +" (Back).pdf"
+
+  try:
+    file(backFile, "rb")
+  except:
+    print "no corresponding back file"
+    return "","",""
+
+  resultsFile = baseName +".pdf"
+  try:
+    aFile = file(resultsFile,"wb")
+  except:
+    print "problems opening results file"
+    return "","",""
+  aFile.close()
+  print frontFile,backFile,resultsFile
+  return frontFile,backFile,resultsFile
+
+def pdfMerger(frontFileName, backFileName, resultsFileName):
+  from pyPdf import PdfFileReader, PdfFileWriter
+
+  pdfFrnt = PdfFileReader(file( frontFileName, "rb"))
+  pdfBack = PdfFileReader(file( backFileName, "rb"))
   
-frntCount = pdfFrnt.getNumPages()
-backCount = pdfBack.getNumPages()
+  frntCount = pdfFrnt.getNumPages()
+  backCount = pdfBack.getNumPages()
 
-if frntCount == backCount:
-  print "We're going to merge"
-  for page in range(frntCount):
-    output.addPage(pdfFrnt.getPage(page))
-    #print page
-    output.addPage(pdfBack.getPage(backCount-page-1))
-    #print backCount-page-1
-else:
-  print "Can't merge"
-   
-outputStream = file(baseFile +".pdf", "wb")
-output.write(outputStream)
-outputStream.close()
+  if frntCount == backCount:
+    pdfResults = file(resultsFileName, "wb")
+    output = PdfFileWriter()
+    for page in range(frntCount):
+      output.addPage(pdfFrnt.getPage(page))
+      output.addPage(pdfBack.getPage(backCount-page-1))
+    output.write(pdfResults)
+    pdfResults.close()
+    return 2*frntCount
+  else:
+    print "page count mismatch: Can't merge"
+    return 0
 
+if __name__ == "__main__":
+  baseName = sys.argv[1]
+  frontName,backName,resultName = baseNameToNamingConvention(baseName)
+  pdfMerger(frontName,backName,resultName)
